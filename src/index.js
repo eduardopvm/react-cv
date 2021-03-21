ebf3eb90391e2229a686f35a40b431c03634c2b5
Eduardo de Moraes <eduardo.palma403@gmail.com>
2021-03-21 05:24:05 -0300

    removed yaml parser, tasked failed successfully

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import yaml from "js-yaml";
 
 import App from "./App";
-
-// fetch("pt-br.yaml")
-//   .then((resp) => resp.text())
-//   .then((text) => {
-//     parsedData = yaml.load(text);
-//     console.log(parsedData);
-//   });
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(

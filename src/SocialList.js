f21ee91dc45be5c0954cff6aabb37c9f892cba2f
Eduardo de Moraes <eduardo.palma403@gmail.com>
2021-03-25 00:29:40 -0300

    adjusted SocialList icon margins

--- src/SocialList.js
+++ src/SocialList.js
@@ -4,15 +4,23 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
+import { makeStyles } from "@material-ui/core/styles";
 
 import { GoMarkGithub } from "react-icons/go";
 import { FaLinkedin, FaFacebookSquare } from "react-icons/fa";
 
+const useStyles = makeStyles({
+  root: {
+    marginRight: 5,
+  },
+});
+
 export default function SocialList(props) {
+  const classes = useStyles();
   return (
     <List>
       <ListItem>
-        <GoMarkGithub style={{ fontSize: 30 }} />
+        <GoMarkGithub style={{ fontSize: 30 }} className={classes.root} />
         <ListItemText>
           <Link href="https://github.com/eduardopvm" target="_blank">
             Github
@@ -21,7 +29,7 @@ export default function SocialList(props) {
       </ListItem>
       <Divider variant="middle" />
       <ListItem>
-        <FaLinkedin style={{ fontSize: 30 }} />
+        <FaLinkedin style={{ fontSize: 30 }} className={classes.root} />
         <ListItemText>
           <Link href="https://www.linkedin.com/in/eduardo-pvm/" target="_blank">
             LinkedIn
@@ -30,7 +38,7 @@ export default function SocialList(props) {
       </ListItem>
       <Divider variant="middle" />
       <ListItem>
-        <FaFacebookSquare style={{ fontSize: 30 }} />
+        <FaFacebookSquare style={{ fontSize: 30 }} className={classes.root} />
         <ListItemText>
           <Link href="#" target="_blank">
             Facebook

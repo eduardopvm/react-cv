f57eff74f22ec3c15a9a482bc699482618f364f5
Eduardo de Moraes <eduardo.palma403@gmail.com>
2021-03-25 01:59:24 -0300

    improving printability of SocialList

--- src/SocialList.js
+++ src/SocialList.js
@@ -1,4 +1,5 @@
 import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 import Link from "@material-ui/core/Link";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
@@ -22,27 +23,48 @@ export default function SocialList(props) {
       <ListItem>
         <GoMarkGithub style={{ fontSize: 30 }} className={classes.root} />
         <ListItemText>
-          <Link href="https://github.com/eduardopvm" target="_blank">
-            Github
-          </Link>
+          <Box displayPrint="none">
+            <Link href="https://github.com/eduardopvm" target="_blank">
+              Github
+            </Link>
+          </Box>
+          <Box display="none" displayPrint="block">
+            <Typography variant="subtitle2">
+              www.github.com/eduardopvm
+            </Typography>
+          </Box>
         </ListItemText>
       </ListItem>
       <Divider variant="middle" component="li" />
       <ListItem>
         <FaLinkedin style={{ fontSize: 30 }} className={classes.root} />
         <ListItemText>
-          <Link href="https://www.linkedin.com/in/eduardo-pvm/" target="_blank">
-            LinkedIn
-          </Link>
+          <Box displayPrint="none">
+            <Link href="https://www.linkedin.com/in/eduardo-pvm/" target="_blank">
+              LinkedIn
+            </Link>
+          </Box>
+          <Box display="none" displayPrint="block">
+            <Typography variant="subtitle2">
+              www.linkedin.com/in/eduardo-pvm
+            </Typography>
+          </Box>
         </ListItemText>
       </ListItem>
       <Divider variant="middle" component="li" />
       <ListItem>
         <FaFacebookSquare style={{ fontSize: 30 }} className={classes.root} />
         <ListItemText>
-          <Link href="#" target="_blank">
-            Facebook
-          </Link>
+          <Box displayPrint="none">
+            <Link href="#" target="_blank">
+              Facebook
+            </Link>
+          </Box>
+          <Box display="none" displayPrint="block">
+            <Typography variant="subtitle2">
+              #
+            </Typography>
+          </Box>
         </ListItemText>
       </ListItem>
       <Divider variant="middle" component="li" />

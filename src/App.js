705e9e31e96d9ba1d6c73eea76a0ae00bd4786b7
Eduardo de Moraes <eduardo.palma403@gmail.com>
2021-03-27 14:09:42 -0300

    renamed PT json file and added empty EN file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 import Layout from "./Layout";
-import cvData from "./data/cv_data_pt-br.json";
+import cvDataPT from "./data/cv_data_pt.json";
+import cvDataEN from "./data/cv_data_en.json";
 
 export default function App() {
   return (
     <React.Fragment>
-      <Layout data={cvData} />
+      <Layout data={cvDataPT} />
     </React.Fragment>
   );
 }

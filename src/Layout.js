e2b1a635a3fc6aa600b24559474830e03b8e9a19
Eduardo de Moraes <eduardo.palma403@gmail.com>
2021-04-10 23:35:33 -0300

    added avatar picture

--- src/Layout.js
+++ src/Layout.js
@@ -1,13 +1,14 @@
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
 import Paper from "@material-ui/core/Paper";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import i18n from "i18next";
-import { useRef } from 'react'
+import { useRef } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { useReactToPrint } from 'react-to-print';
-import { useTranslation } from 'react-i18next';
+import { useReactToPrint } from "react-to-print";
+import { useTranslation } from "react-i18next";
 
 import Header from "./Header";
 import Sidebar from "./Sidebar";
@@ -16,17 +17,13 @@ import React from "react";
 
 // TODO: refactor this
 const useStyles = makeStyles((theme) => ({
-  small: {
-    width: theme.spacing(7),
-    height: theme.spacing(7),
-  },
-  large: {
-    width: theme.spacing(20),
-    height: theme.spacing(20),
+  avatar: {
+    width: theme.spacing(28),
+    height: theme.spacing(28),
   },
   button: {
-    margin: "0.5rem"
-  }
+    margin: "0.5rem",
+  },
 }));
 
 export default function Layout(props) {
@@ -47,41 +44,84 @@ export default function Layout(props) {
     <Container fixed>
       <Grid container>
         <Grid item xs={6}>
-          <Button onClick={handlePrint} variant="contained" color="primary" className={classes.button}>{t('printCV')}</Button>
+          <Button
+            onClick={handlePrint}
+            variant="contained"
+            color="primary"
+            className={classes.button}
+          >
+            {t("printCV")}
+          </Button>
         </Grid>
         <Grid container item xs={6} justify="flex-end">
-          {i18n.language == 'pt'
-            ? (
-              <React.Fragment>
-                <Button onClick={() => changeLanguage('pt')} disabled className={classes.button}>Português</Button>
-                <Button onClick={() => changeLanguage('en')} color="primary" className={classes.button}>English</Button>
-              </React.Fragment>
-            )
-            : (
-              <React.Fragment>
-                <Button onClick={() => changeLanguage('pt')} color="primary" className={classes.button}>Português</Button>
-                <Button onClick={() => changeLanguage('en')} disabled className={classes.button}>English</Button>
-              </React.Fragment>
-            )
-          }
+          {i18n.language == "pt" ? (
+            <React.Fragment>
+              <Button
+                onClick={() => changeLanguage("pt")}
+                disabled
+                className={classes.button}
+              >
+                Português
+              </Button>
+              <Button
+                onClick={() => changeLanguage("en")}
+                color="primary"
+                className={classes.button}
+              >
+                English
+              </Button>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              <Button
+                onClick={() => changeLanguage("pt")}
+                color="primary"
+                className={classes.button}
+              >
+                Português
+              </Button>
+              <Button
+                onClick={() => changeLanguage("en")}
+                disabled
+                className={classes.button}
+              >
+                English
+              </Button>
+            </React.Fragment>
+          )}
         </Grid>
       </Grid>
-      
+
       <Paper elevation={5} component="main">
         {/* Full page grid container */}
-        <Grid container ref={componentRef} style={{ paddingTop: "1rem"}}>
+        <Grid container ref={componentRef} style={{ paddingTop: "1rem" }}>
           <Grid container item xs={4}>
             <Grid container item justify="center" alignItems="center">
-              <Avatar alt="Foto do Perfil" className={classes.large}>
-                EPVM
-              </Avatar>
+              <Box displayPrint="none">
+                <Avatar
+                  alt="Foto de Eduardo P. V. de Moraes"
+                  className={classes.avatar}
+                  src={process.env.PUBLIC_URL + "/gray_avatar.jpg"}
+                >
+                  EPVM
+                </Avatar>
+              </Box>
+              <Box display="none" displayPrint="block">
+                {/* TODO: add QRCode instead */}
+                <Avatar className={`print-avatar ` + classes.avatar}>
+                  Eduardo
+                </Avatar>
+              </Box>
             </Grid>
           </Grid>
           <Grid container item xs={8}>
             <Header data={props.staticData.contact} />
           </Grid>
           <Grid item xs={4}>
-            <Sidebar staticData={props.staticData} translatedData={props.translatedData} />
+            <Sidebar
+              staticData={props.staticData}
+              translatedData={props.translatedData}
+            />
           </Grid>
           <Grid item xs={8}>
             <MainContent data={props.translatedData} />

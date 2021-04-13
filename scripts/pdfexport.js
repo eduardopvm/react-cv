3d850bbbf3a1c186532650925ab37ed4b8619ee4
Eduardo de Moraes <eduardo.palma403@gmail.com>
2021-04-12 22:42:20 -0300

    WIP pdf generation

--- scripts/pdfexport.js
+++ scripts/pdfexport.js
@@ -1,14 +1,29 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('http://localhost:3000', {
-    waitUntil: 'networkidle2',
+  await page.goto("http://localhost:3000", {
+    waitUntil: "networkidle2",
   });
-  await page.emulateMediaType('screen');
-  await page.$("#pdf-target");
-  await page.pdf({ path: 'page_export.pdf', format: 'a4', printBackground: true });
+  await page.emulateMediaType("screen");
+
+  const pdfTarget = await page.$("#pdf-target");
+  await page.evaluate((el) => {
+    el = el.cloneNode(true);
+
+    document.body.innerHTML = `
+    <div>
+      ${el.outerHTML}
+    </div>
+  `;
+  }, pdfTarget);
+
+  await page.pdf({
+    path: "page_export.pdf",
+    format: "a4",
+    printBackground: true,
+  });
 
   await browser.close();
 })();

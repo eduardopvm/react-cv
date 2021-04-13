a4d454c2a549432db0584d3d2bfb703cda8182a1
Eduardo de Moraes <eduardo.palma403@gmail.com>
2021-04-12 23:04:25 -0300

    messing around with puppeteers options

--- scripts/pdfexport.js
+++ scripts/pdfexport.js
@@ -3,6 +3,9 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  
+  // await page.setViewport({width: 1200, height: 800, deviceScaleFactor: 2});
+  
   await page.goto("http://localhost:3000", {
     waitUntil: "networkidle2",
   });
@@ -13,16 +16,19 @@ const puppeteer = require("puppeteer");
     el = el.cloneNode(true);
 
     document.body.innerHTML = `
-    <div>
+    <!DOCTYPE html>
+    <html>
+    <body>
       ${el.outerHTML}
-    </div>
+    </body>
+    </html>
   `;
   }, pdfTarget);
 
   await page.pdf({
     path: "page_export.pdf",
-    format: "a4",
-    printBackground: true,
+    // format: "a4",
+    // printBackground: true,
   });
 
   await browser.close();

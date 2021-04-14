4d44f9e1494308655491a367da12c7f3411ce8ab
Eduardo de Moraes <eduardo.palma403@gmail.com>
2021-04-14 14:21:29 -0300

    translated another label I missed

--- src/sidebar/CertificateList.js
+++ src/sidebar/CertificateList.js
@@ -7,10 +7,12 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import IconButton from "@material-ui/core/IconButton";
+import { useTranslation } from "react-i18next";
 
 import { GoLinkExternal } from "react-icons/go";
 
 export default function CertificateList(props) {
+  const [t, i18n] = useTranslation();
   const certList = props.certificates.map((cert) => {
     return (
       <Grid container key={cert.name}>
@@ -19,9 +21,7 @@ export default function CertificateList(props) {
             <ListItemText>
               <Box>
                 <Typography>{cert.name}</Typography>
-                <Typography display="inline" color="textSecondary">
-                  Emissor: {cert.issuer}
-                </Typography>
+                <Typography color="textSecondary">{t("issuer")}: {cert.issuer}</Typography>
                 <Box display="none" displayPrint="block">
                   {cert.url != null &&
                     <Typography color="textSecondary">URL: {cert.url}</Typography>
@@ -30,6 +30,8 @@ export default function CertificateList(props) {
               </Box>
             </ListItemText>
           </Grid>
+
+          {/* List Button */}
           <Grid item xs={3}>
             <ListItemSecondaryAction>
               <Box displayPrint="none">

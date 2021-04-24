2b0ff571db56543e649802b74e55316ce2852684
Eduardo de Moraes <eduardo.palma403@gmail.com>
2021-04-24 20:15:35 -0300

    preparing header for mobile appBar

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
-import Backdrop from '@material-ui/core/Backdrop';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Snackbar from '@material-ui/core/Snackbar';
-import MuiAlert from '@material-ui/lab/Alert';
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
 import { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { useTranslation } from "react-i18next";
@@ -15,7 +17,7 @@ const useStyles = makeStyles((theme) => ({
   },
   backdrop: {
     zIndex: theme.zIndex.drawer + 1,
-    color: '#fff',
+    color: "#fff",
   },
 }));
 
@@ -77,71 +79,77 @@ export default function Header(props) {
   };
 
   const hideErrorMessage = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
     showError(false);
-  }
+  };
 
   return (
-    <Grid container component="header">
-      <Backdrop className={classes.backdrop} open={loading}>
-        <CircularProgress color="inherit" />
-      </Backdrop>
-      <Snackbar open={errorHappened} onClose={hideErrorMessage} autoHideDuration={7000}>
-        <Alert onClose={hideErrorMessage} severity="error">
-          {t("exportToPDFError")}
-        </Alert>
-      </Snackbar>
-      <Grid item xs={6}>
-        <Button
-          onClick={onPagePrint}
-          variant="contained"
-          color="primary"
-          className={classes.button}>
-          {t("printCV")}
-        </Button>
-        <Button
-          onClick={onPdfExport}
-          variant="contained"
-          color="primary"
-          className={classes.button}>
-          {t("exportToPDF")}
-        </Button>
+    <AppBar position="static" elevation={0} color="transparent">
+      <Grid container>
+        <Backdrop className={classes.backdrop} open={loading}>
+          <CircularProgress color="inherit" />
+        </Backdrop>
+        <Snackbar
+          open={errorHappened}
+          onClose={hideErrorMessage}
+          autoHideDuration={7000}>
+          <Alert onClose={hideErrorMessage} severity="error">
+            {t("exportToPDFError")}
+          </Alert>
+        </Snackbar>
+        <Grid item xs={6}>
+          <Button
+            onClick={onPagePrint}
+            variant="contained"
+            color="primary"
+            className={classes.button}>
+            {t("printCV")}
+          </Button>
+          <Button
+            onClick={onPdfExport}
+            variant="contained"
+            color="primary"
+            className={classes.button}>
+            {t("exportToPDF")}
+          </Button>
+        </Grid>
+        
+        <Grid container item xs={6} justify="flex-end">
+          {i18n.language == "pt" ? (
+            <React.Fragment>
+              <Button
+                onClick={() => onChangeLanguage("pt")}
+                disabled
+                className={classes.button}>
+                Português
+              </Button>
+              <Button
+                onClick={() => onChangeLanguage("en")}
+                color="primary"
+                className={classes.button}>
+                English
+              </Button>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              <Button
+                onClick={() => onChangeLanguage("pt")}
+                color="primary"
+                className={classes.button}>
+                Português
+              </Button>
+              <Button
+                onClick={() => onChangeLanguage("en")}
+                disabled
+                className={classes.button}>
+                English
+              </Button>
+            </React.Fragment>
+          )}
+        </Grid>
       </Grid>
-      <Grid container item xs={6} justify="flex-end">
-        {i18n.language == "pt" ? (
-          <React.Fragment>
-            <Button
-              onClick={() => onChangeLanguage("pt")}
-              disabled
-              className={classes.button}>
-              Português
-            </Button>
-            <Button
-              onClick={() => onChangeLanguage("en")}
-              color="primary"
-              className={classes.button}>
-              English
-            </Button>
-          </React.Fragment>
-        ) : (
-          <React.Fragment>
-            <Button
-              onClick={() => onChangeLanguage("pt")}
-              color="primary"
-              className={classes.button}>
-              Português
-            </Button>
-            <Button
-              onClick={() => onChangeLanguage("en")}
-              disabled
-              className={classes.button}>
-              English
-            </Button>
-          </React.Fragment>
-        )}
-      </Grid>
-    </Grid>
+    </AppBar>
   );
 }

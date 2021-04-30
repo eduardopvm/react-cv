48317304f0ee176d44aa1f6c35f4d1c6ee36e975
Eduardo de Moraes <eduardo.palma403@gmail.com>
2021-04-30 20:30:51 -0300

    fixed infinite render bug when loading the EN version through the query string; also added preparation for loading the CV data from the API

--- src/main_content/Homepage.js
+++ src/main_content/Homepage.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
-import { useState, useRef } from "react";
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { useState, useEffect, useRef } from "react";
 import { useReactToPrint } from "react-to-print";
 import { useTranslation } from "react-i18next";
 import { HashRouter as Router, Switch, Route, useLocation } from "react-router-dom";
@@ -14,24 +16,35 @@ import cvDataPT from "../data/cv_data_pt.json";
 import cvDataEN from "../data/cv_data_en.json";
 import cvDataStatic from "../data/cv_data_static.json";
 
+// TODO: detect browser language instead of loading PT by default
+
 export default function Homepage() {
   const [t, i18n] = useTranslation();
-  const [cvData, setData] = useState(getData());
+  const [cvData, setData] = useState();
+  const [isLoaded, setIsLoaded] = useState(false);
   const printComponentRef = useRef();
+  const searchParam = useLocation().search;
 
-  function getData() {
-    let queryParams = new URLSearchParams(useLocation().search);
+  useEffect(() => {
+    setData(loadCVData());
+    setIsLoaded(true);
+  }, []);
+
+  // For now this is only necessary for the PDF export feature
+  // But it's already a preparation for loading the data from the API in the future
+  function loadCVData() {
+    let queryParams = new URLSearchParams(searchParam);
     let lang = queryParams.get("lang");
 
     if (lang && lang == "en") {
       i18n.changeLanguage("en");
       return cvDataEN;
+    } else {
+      return cvDataPT;
     }
-
-    return cvDataPT;
   }
 
-  function handleDataChange(lang) {
+  function handleLanguageChange(lang) {
     if (lang == "pt") {
       setData(cvDataPT);
     } else {
@@ -44,22 +57,32 @@ export default function Homepage() {
     content: () => printComponentRef.current,
   });
 
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Container disableGutters>
-            <Header handleLanguageChange={handleDataChange} handlePagePrint={handlePagePrint} history={cvData.history} />
-            <Paper elevation={5} component="main" square>
-              <Content ref={printComponentRef} staticData={cvDataStatic} translatedData={cvData} />
-            </Paper>
-            <Footer githubUrl={cvDataStatic.source_url} />
-          </Container>
-        </Route>
-        <Route path="/pdf">
-          <Content staticData={cvDataStatic} translatedData={cvData} />
-        </Route>
-      </Switch>
-    </Router>
-  );
+  if (!isLoaded) {
+    return (
+      <div>
+        <Backdrop open={!isLoaded}>
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      </div>
+    );
+  } else {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Container disableGutters>
+              <Header handleLanguageChange={handleLanguageChange} handlePagePrint={handlePagePrint} history={cvData.history} />
+              <Paper elevation={5} component="main" square>
+                <Content ref={printComponentRef} staticData={cvDataStatic} translatedData={cvData} />
+              </Paper>
+              <Footer githubUrl={cvDataStatic.source_url} />
+            </Container>
+          </Route>
+          <Route path="/pdf">
+            <Content staticData={cvDataStatic} translatedData={cvData} />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
